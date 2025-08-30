baccd973662eac4c4687ab7e039251158befc4d9
Galil-ux <129039607+Galil-ux@users.noreply.github.com>
2025-08-30 09:57:58 +0300

    Create math_ops.c

---
+++ math_ops.c
@@ -0,0 +1,31 @@
+// math_ops.c
+#include <math.h>
+
+// Scientific functions
+double c_sin(double x) {
+    return sin(x);
+}
+
+double c_cos(double x) {
+    return cos(x);
+}
+
+double c_tan(double x) {
+    return tan(x);
+}
+
+double c_log(double x) {
+    return log(x);
+}
+
+double c_sqrt(double x) {
+    return sqrt(x);
+}
+
+double c_pow(double x, double y) {
+    return pow(x, y);
+}
+
+double c_exp(double x) {
+    return exp(x);
+}
